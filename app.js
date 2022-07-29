2472230e10085a5672834f847c08aeca1905beb5
ljk <3104653373@qq.com>
2022-07-29 15:41:19 +0800

    修改控制台提示信息

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(3000, () => {
-  console.log("express start at http://127.0.0.1:3000");
+  console.log("express start at http://127.0.0.1:3000/api");
 });
